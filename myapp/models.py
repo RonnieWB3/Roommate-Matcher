3321ef2434f5d5a105d5b17cd162d0932fb20a29
Natan Najera <125765564+Kronera@users.noreply.github.com>
2024-10-05 02:50:48 -0700

    superuser/admin created
    Add TestModel for testing admin functionality

--- myapp/models.py
+++ myapp/models.py
@@ -1,6 +1,7 @@
 from django.db import models
 
-class TestMode(models.Model):
+class TestModel(models.Model):
+    # This model was used to test connections and admin functionality.
     name = models.CharField(max_length=100)
     description = models.TextField()
 
